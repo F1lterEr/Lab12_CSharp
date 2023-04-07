10b61c1a47721b79f3e1093eba1b0127a3111129
F1lterEr <41ilter.er@gmail.com>
2023-04-07 11:14:13 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class FirstClass
+{
+    public event Action<string> CustomEvent;
+
+    public void RaiseEvent(string objectName)
+    {
+        CustomEvent?.Invoke(objectName);
+    }
+}
+
+class SecondClass
+{
+    public void HandleEvent(string objectName)
+    {
+        Console.WriteLine($"Обработанное событие для {objectName}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        FirstClass obj1 = new FirstClass();
+        FirstClass obj2 = new FirstClass();
+        SecondClass handler = new SecondClass();
+
+        obj1.CustomEvent += handler.HandleEvent;
+        obj2.CustomEvent += handler.HandleEvent;    
+
+        obj1.RaiseEvent("Объект 1");
+        obj2.RaiseEvent("Объект 2");
+
+        Console.ReadLine();
+    }
+}
